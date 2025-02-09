7516dffaec060a5ba482766b4bdcddbd78a384ae
IQiyuu <60314258+IQiyuu@users.noreply.github.com>
2025-02-09 13:09:32 +0100

    Update run.sh

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -4,11 +4,11 @@
 service mysql start
 sleep 5
 # creer la database '$SQL_DB' si elle n existe pas deja
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};"
 # pareil qu au dessus mais avec un user + son password
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 # on lui donne tous les privileges histoire qu'il puisse manipule la table (.* toutes les tables de la db) 
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 # on change le mot de pass du root
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 # on update les changements fait au dessus
