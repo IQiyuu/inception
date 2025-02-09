a3b8844a801161b9e6111d25f3270b7d691697e8
IQiyuu <60314258+IQiyuu@users.noreply.github.com>
2025-02-09 13:01:15 +0100

    Update config.sh

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -29,15 +29,24 @@ mkdir -p /run/php/;
 touch /run/php/php7.3-fpm.pid;
 
 if [ ! -f /var/www/html/wp-config.php ]; then
+	echo "Wordpress configuration..."
 	mkdir -p /var/www/html
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 	chmod +x wp-cli.phar; 
 	mv wp-cli.phar /usr/local/bin/wp;
 	cd /var/www/html;
+ 	echo "Wp core download"
 	wp core download --allow-root;
-	mv /var/www/wp-config.php /var/www/html/
+ 	echo "Wp config create"
+ 	wp config create	--allow-root \
+ 				--dbname=$MYSQL_DB \
+ 				--dbuser=$MYSQL_USER \
+ 				--dbpass=$MYSQL_PASSWORD \
+ 				--dbhost=mariadb:3306 --path='/var/www/html'
+     	echo "Wp core install"
 	wp core install --allow-root --url=${localhost} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_MAIL}
-	wp user create --allow-root ${WP_USER} ${WP_USER_MAIL} --user_pass=${WP_USER_PASSWORD};
+	echo "Wp user create"
+ 	wp user create --allow-root ${WP_USER} ${WP_USER_MAIL} --user_pass=${WP_USER_PASSWORD};
 fi
 
 echo "Wordpress end"
